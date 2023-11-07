8ab937ba40c83b08cdf9bae1e87ba7ee9bec8291
Nathan <nathan.salbego@gmail.com>
2023-11-07 14:28:46 +0100

    Test

--- src/test/TestAffichage_.java
+++ src/test/TestAffichage_.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.Test;
 
 class TestAffichage_ {
 	
+	@Test
 	void testAffichePlace() {
 		Place p=new Place(1);
 		assertEquals(p.affichage(),"place avec 1 jeton");
 		Place p2 = new Place(2);
 		assertEquals(p2.affichage(),"place avec 2 jetons");
 	}
-	
+	@Test
 	void testAfficheTransition() {
 		Transition t = new Transition();
 		assertEquals(t.affichage(),"transition, "+0+" arcs entrants, "+0+" arcs sortants");
@@ -22,13 +23,17 @@ class TestAffichage_ {
 		t.ajouter_out(eO);
 		assertEquals(t.affichage(),"transition, "+1+" arcs entrants, "+1+" arcs sortants");
 	}
-	
+	@Test
 	void testAfficheArc() {
-		Edge_in e =new Edge_in(2);
-		assertEquals(e.affichage(),"arc de poids "+2);
-		Edge_out eO = new Edge_out(3);
-		assertEquals(e0.affichage(),"arc de poids 3 (place avec "+super.getPlace().getJeton()+" jetons vers transition")
+		Place p = new PLace(3);
+		Edge_in e =new Edge_in(2,p);
+		assertEquals(e.affichage(),"arc de poids "+2+" (place avec 3 jetons vers transition)");
+		Place p = new PLace(3);
+		Edge_out eO = new Edge_out(3,p);
+		
+		assertEquals(e0.affichage(),"arc de poids 3 (place avec 3 jetons vers transition)");
 	}
+	@Test
 	void testAfficheReseau() {
 		Petrinet pn = new Petrinet();
 		
