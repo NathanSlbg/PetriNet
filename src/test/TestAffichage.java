2f679aabf1fadfec14ccd533ef12a415d4de0767
Nathan <nathan.salbego@gmail.com>
2023-11-07 13:53:58 +0100

    Tests

--- src/test/TestAffichage.java
+++ src/test/TestAffichage.java
@@ -1,5 +1,38 @@
 package src.test;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.MethodOrderer.*;
 
-public class TestAffichage {
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+class TestAffichage {
+	
+	void testAffichePlace() {
+		Place p=new Place(1);
+		assertEquals(p.affichage(),"place avec 1 jeton");
+		Place p2 = new Place(2);
+		assertEquals(p2.affichage(),"place avec 2 jetons");
+	}
+	
+	void testAfficheTransition() {
+		Transition t = new Transition();
+		assertEquals(t.affichage(),"transition, "+0+" arcs entrants, "+0+" arcs sortants");
+		Edge_in eI = new Edge_in(2);
+		t.ajouter_in(eI);
+		assertEquals(t.affichage(),"transition, "+1+" arcs entrants, "+0+" arcs sortants");
+		Edge_out eO = new Edge_out(3);
+		t.ajouter_out(eO);
+		assertEquals(t.affichage(),"transition, "+1+" arcs entrants, "+1+" arcs sortants");
+	}
+	
+	void testAfficheArc() {
+		Edge_in e =new Edge_in(2);
+		assertEquals(e.affichage(),"arc de poids "+2);
+	}
+	void testAfficheReseau() {
+		Petrinet pn = new Petrinet();
+		
+	}
 
 }
